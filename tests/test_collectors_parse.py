6c9ee6719bc57dd6974d88c21e64c6742a165201
jurraca <julienu@pm.me>
2025-04-07 15:31:50 +0100

    add specific parse cases

--- tests/test_collectors_parse.py
+++ tests/test_collectors_parse.py
@@ -1,9 +1,8 @@
 from pathlib import Path
+import shutil
 
 from kartograf.collectors.parse import parse_routeviews_pfx2as
 from .context import create_test_context
-
-import shutil
 
 def build_test_context(tmp_path):
     epoch = "111111112"
@@ -27,3 +26,11 @@ def test_parse(tmp_path):
 
     assert result_file.exists()
 
+    with open(result_file, 'r') as f:
+        results = [l.strip() for l in f.readlines()]
+
+    assert "127.0.0.0/24 AS12345" not in results
+    assert "2001:db8::/32 AS54321" not in results
+    assert "2ab1:db8::/32 AS33521665" not in results
+
+    assert results == ["1.0.0.0/24 AS13335", "1.0.4.0/24 AS38803", "1.0.16.0/24 AS2519"]
